2dc792e100e8ad9d482a816c3ff3399e7aa9166f
Chuma Okoro <cokoro@etsy.com>
2019-09-08 19:31:18 -0400

    add toolbar to display message activity

--- app/src/main/java/com/example/thefirstofchuma/DisplayMessageActivity.java
+++ app/src/main/java/com/example/thefirstofchuma/DisplayMessageActivity.java
@@ -1,6 +1,7 @@
 package com.example.thefirstofchuma;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,6 +13,8 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
